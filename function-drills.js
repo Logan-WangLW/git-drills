d0f9bec2a605909196843f4fde8130d70898ab04
Logan Wang <loganxwang@hotmail.com>
2018-09-24 11:02:15 -0700

    created function

--- function-drills.js
+++ function-drills.js
@@ -1,3 +1,7 @@
 'use strict';
 
-console.log('Hi, my name is Arun and I\'m 31 years old');
+function createGreeting(){
+    console.log('Hi, my name is Arun and I\'m 31 years old');
+}
+
+createGreeting();
