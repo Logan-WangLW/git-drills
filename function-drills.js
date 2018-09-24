189bb82409091bbe47235587e2e9b2db2d7258bd
Logan Wang <loganxwang@hotmail.com>
2018-09-24 11:06:55 -0700

    used function parameters

--- function-drills.js
+++ function-drills.js
@@ -1,8 +1,8 @@
 'use strict';
 
-function createGreeting(){
-  return 'Hi, my name is Arun and I\'m 31 years old';
+function createGreeting(name, age){
+  return `Hi, my name is ${name} and I'm ${age} years old`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting("Arun", 31);
 console.log(greeting1);
