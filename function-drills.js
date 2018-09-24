06d796cd450e86736b7c54e1c4158238da2d791d
Arun Seehra <seehra.a@gmail.com>
2018-09-24 13:28:33 -0500

    Catch negative ages

--- function-drills.js
+++ function-drills.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function getYearOfBirth(age){
-    return 2018 - age;
+  if (age < 0) {
+    throw new Error('Age cannot be negative');
+  }
+
+  return 2018 - age;
 }
 
 function createGreeting(name, age){
@@ -9,5 +13,8 @@ function createGreeting(name, age){
   return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
-const greeting1 = createGreeting("Arun", 31);
-console.log(greeting1);
+try {
+  const greeting1 = createGreeting("Arun", 31);
+  console.log(greeting1);
+} catch (e) {
+}
